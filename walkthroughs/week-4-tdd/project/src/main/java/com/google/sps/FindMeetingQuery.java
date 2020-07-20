439c477c191f17fe308100dc786a23ad2db162c3
Dong Wang <wangdong.donald@gmail.com>
2020-07-20 04:05:17 -0400

    Complete portfolio

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,54 @@
 
 package com.google.sps;
 
+import java.lang.Math;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> attendees = request.getAttendees();
+    int duration = (int)request.getDuration();
+    Collection<TimeRange> availableTimeSlots = timeSlots(events, attendees);
+    
+    return availableTimeSlots.stream().filter(slot -> slot.duration() >= duration).collect(Collectors.toList());
+  }
+
+  private Collection<TimeRange> timeSlots(Collection<Event> events, Collection<String> attendees) {
+    List<TimeRange> slots = new ArrayList<>();
+    for (Event event : events) {
+      if (existsAttendees(event, attendees)) {
+        slots.add(event.getWhen());
+      }
+    }
+    Collections.sort(slots, TimeRange.ORDER_BY_START);
+
+    Collection<TimeRange> availableTimeSlots = new ArrayList<>();
+    int start = TimeRange.START_OF_DAY;
+    for (TimeRange slot : slots) {
+      if (start < slot.start()) {
+        availableTimeSlots.add(TimeRange.fromStartEnd(start, slot.start(), false));
+      }
+      start = Math.max(start, slot.end());
+    }
+    if (start < TimeRange.END_OF_DAY) {
+      availableTimeSlots.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+    }
+    return availableTimeSlots;
+  }
+
+  private boolean existsAttendees(Event event, Collection<String> attendees) {
+    Set<String> eventAttendees = event.getAttendees();
+    for (String attendee : eventAttendees) {
+        if (attendees.contains(attendee)) {
+            return true;
+        }
+    }
+    return false;
   }
 }
